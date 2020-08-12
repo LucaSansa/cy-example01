53261719dd8affcfbde283de19878652de352b71
Lucas Pereira <lucas_pereiracp@outlook.com.br>
2020-08-12 01:53:47 -0300

    implementação do teste para calcular imposto

--- cypress/integration/primeiro_test.spec.js
+++ cypress/integration/primeiro_test.spec.js
@@ -5,7 +5,7 @@ describe('Calculadora de imposto', () => {
         cy.get('[name=idade]').type('20');
         cy.get('[name=sexo]').select('M');
         cy.get('[name=salario]').type('2000');
-        // cy.get(`[value='Limpar campos']`).click();
+        cy.get(`[value='Limpar campos']`).click();
 
         cy.get('[name=nome]').should('have.text', '');
         cy.get('[name=idade]').should('have.text', '');
@@ -15,9 +15,14 @@ describe('Calculadora de imposto', () => {
 
     it('deve calcular o imposto devido', () => {
         // preencha todos os campos
-
+        cy.get('[name=nome]').type('Lucas');
+        cy.get('[name=idade]').type('26');
+        cy.get('[name=sexo]').select('M');
+        cy.get('[name=salario]').type('2000');
         // clique no botao calcular
+        cy.get(`[value='Calcular']`).click();
 
         // verifique (usando .should) a saída retornada com o calculo do imposto devido
+        cy.get('[id="divValorImposto"]').should('have.text', 'Valor a pagar de imposto: 300');
     });
 });
